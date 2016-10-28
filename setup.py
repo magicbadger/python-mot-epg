816f706985ec18b82a8ab09a9247b69cc78045db
Ben Poor <ben.poor@thisisglobal.com>
2016-10-28 20:44:27 +0100

    tweak for pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='motepg',
-      version='1.0.0',
+      version='1.0.1',
       description='Plugin to the python-mot library to handle DAB EPG binary encoding as per ETSI TS 102 371', 
       author='Ben Poor',
       author_email='ben.poor@thisisglobal.com',
       url='https://github.com/GlobalRadio/python-mot-epg',
-      download_url='https://github.com/GlobalRadio/python-mot-epg/tarball/1.0.0',
+      download_url='https://github.com/GlobalRadio/python-mot-epg/tarball/1.0.1',
       keywords=['dab', 'epg'],
       packages=['mot', 'mot.epg'],
       package_dir = {'' : 'src'}
