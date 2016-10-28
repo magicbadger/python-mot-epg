4b84f1f4132421e0320fbb30308479814884230b
Ben Poor <ben.poor@thisisglobal.com>
2016-10-28 21:52:44 +0100

    pypi

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='dabmotepg',
-      version='1.0.2',
+      version='1.0.3',
       description='Plugin to the python-mot library to handle DAB EPG binary encoding as per ETSI TS 102 371', 
       author='Ben Poor',
       author_email='ben.poor@thisisglobal.com',
       url='https://github.com/GlobalRadio/python-mot-epg',
-      download_url='https://github.com/GlobalRadio/python-mot-epg/tarball/1.0.2',
+      download_url='https://github.com/GlobalRadio/python-mot-epg/tarball/1.0.3',
       keywords=['dab', 'epg'],
-      packages=['mot', 'mot.epg'],
+      packages=['mot.epg'],
       package_dir = {'' : 'src'}
      )
